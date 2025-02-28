7da48cd024472bc3c136b0e585bca8e8d1a54b90
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-03-01 05:29:39 +0600

    Activity Log Added

--- JavaScript/dateTime.js
+++ JavaScript/dateTime.js
@@ -1,14 +1,27 @@
-document.getElementById('date-time');
+
+// Adding the time
+
+const dateTimeToday = new Date();
+let hour = dateTimeToday.getHours();
+const minute = dateTimeToday.getMinutes();
+const second = dateTimeToday.getSeconds();
+const period = hour >= 12 ? 'PM' : 'AM';
+hour = hour % 12 || 12;
+
+// console.log(hour, minute, second, period)
 
 
-const dateTimeToday =new Date();
+
 // Getting Day, Month, Date and Year
-const day =dateTimeToday.toLocaleString('default',{weekday:'short'});
-const month =dateTimeToday.toLocaleString('default',{month:'short'});
+const day = dateTimeToday.toLocaleString('default', { weekday: 'short' });
+const month = dateTimeToday.toLocaleString('default', { month: 'short' });
 const date = dateTimeToday.getDate();
 const year = dateTimeToday.getFullYear();
 
-document.getElementById('date-time').innerHTML=`
+document.getElementById('date-time').innerHTML = `
 <span class='text-lg'>${day}</span>,</br>
 <span class='font-bold'>${month} ${date} ${year}
 </span>`
+
+
+
---
+++ JavaScript/main.js
@@ -0,0 +1,62 @@
+const workButtons = document.querySelectorAll('.working-button');
+// const workTitles = document.querySelectorAll('.work-title');
+let clickCount = 0;
+let leftCount = 6;
+let rightCount = 23;
+
+
+for(let i = 0; i < workButtons.length;i++){
+    const workBtn = workButtons[i];
+    workBtn.addEventListener('click',function(event){
+        event.preventDefault();
+        const workTitle = document.getElementsByClassName('work-title');
+        const workTitleInnerText=workTitle[0].innerText;
+       
+        
+        if(!workBtn.disabled === true){
+            clickCount= clickCount+1;
+            workBtn.setAttribute('disabled','true');
+            workBtn.classList.add('bg-slate-200');
+            alert('Board Updated Successfully!');
+
+            // Adding Work Title
+            // const workTitle = document.getElementsByClassName('work-title').innerText;
+            // console.log(workTitle)
+            // Removing count from left div
+            leftCount--;
+            document.getElementById('left-count').innerText=leftCount;
+            
+            // Adding Count to the right counter
+            rightCount++;
+            document.getElementById('right-count').innerText= rightCount;
+
+
+            
+
+            const container = document.getElementById('activity-div');
+            const div =document.createElement('div');
+            div.innerHTML=`
+            <h1 class="bg-[#F4F7FF] p-4 m-4 text-sm rounded-xl">You Have completed the task "<span class="font-bold">${workTitleInnerText}</span>" at ${hour}:${minute}:${second} ${period}</h1>`
+            // container.appendChild(div)
+            container.insertBefore(div, container.firstChild);
+            
+        }
+        if(clickCount === 6){
+            alert('Congrats!!! You Have Completed All the current task');
+
+        }
+
+        
+
+
+        // console.log(workBtn)
+        // console.log(typeof workBtn)
+        
+    })
+}
+// Clearing the Activity Log
+document.getElementById('activity-completed').addEventListener('click',function () {
+    const container = document.getElementById('activity-div');
+    container.innerHTML='';
+    
+})
